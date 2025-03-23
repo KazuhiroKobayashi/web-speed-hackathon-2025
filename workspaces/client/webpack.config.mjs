f52a9ac684421142054c0b2ea945458b4bf68814
Kazuhiro Kobayashi <kkobayashi.oa@gmail.com>
2025-03-23 23:45:16 +0900

    chore: change webpack configuration.

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -5,62 +5,42 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   entry: './src/main.tsx',
-  mode: 'none',
+  mode: 'production',
   module: {
     rules: [
       {
         exclude: [/node_modules\/video\.js/, /node_modules\/@videojs/],
-        resolve: {
-          fullySpecified: false,
-        },
+        resolve: { fullySpecified: false },
         test: /\.(?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              [
-                '@babel/preset-env',
-                {
-                  corejs: '3.41',
-                  forceAllTransforms: true,
-                  targets: 'defaults',
-                  useBuiltIns: 'entry',
-                },
-              ],
+              ['@babel/preset-env', { corejs: '3.41', targets: { chrome: '134' } }],
               ['@babel/preset-react', { runtime: 'automatic' }],
               ['@babel/preset-typescript'],
             ],
           },
         },
       },
-      {
-        test: /\.png$/,
-        type: 'asset/inline',
-      },
-      {
-        resourceQuery: /raw/,
-        type: 'asset/source',
-      },
+      { test: /\.(avif|jpeg|png)$/, type: 'asset' },
+      { resourceQuery: /raw/, type: 'asset/source' },
       {
         resourceQuery: /arraybuffer/,
         type: 'javascript/auto',
-        use: {
-          loader: 'arraybuffer-loader',
-        },
+        use: { loader: 'arraybuffer-loader' },
       },
     ],
   },
   output: {
     chunkFilename: 'chunk-[contenthash].js',
-    chunkFormat: false,
     filename: 'main.js',
     path: path.resolve(import.meta.dirname, './dist'),
     publicPath: 'auto',
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
     new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
   ],
